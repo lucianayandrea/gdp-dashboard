9f30975eec6585b8eb5bd1ff031c533b5a4a3671
lucianayandrea <lucirey04@gmail.com>
2024-11-19 14:56:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from pathlib import Path
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='Proyecto Bioinformatica',
-    page_icon=':hearth_emoji:', # This is an emoji shortcode. Could be a URL too.
+    page_icon=':earth_world:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
