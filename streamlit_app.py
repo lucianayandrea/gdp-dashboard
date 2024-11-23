fa9f3f4996a6dd497963f74212cfb210b76d6ada
lucianayandrea <lucirey04@gmail.com>
2024-11-22 22:33:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,7 @@ proteinas = [
     {
         "nombre": "Insulina",
         "descripcion": "La insulina es una hormona que produce el páncreas, ayuda al cuerpo a utilizar y almacenar la glucosa, o azúcar en la sangre, como fuente de energía .",
-        "imagen": "https://upload.wikimedia.org/wikipedia/commons/6/67/Dynamin.
+        "imagen": 
    },
     {
         "nombre": "Colágeno",
