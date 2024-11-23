3f673534d86e6b096cb4ddcbf80ed8bd5b3c639a
lucianayandrea <lucirey04@gmail.com>
2024-11-22 20:35:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,6 +166,8 @@ if st.button("Transcribir ADN a ARN"):
 # -----------------------------------------------------------------------------
 # Diccionario de codones de ARN a aminoácidos
 codon_to_aminoacid = {
+# Diccionario de codones de ARN a aminoácidos
+codon_to_aminoacid = {
     "AUG": "Met", "UUU": "Phe", "UUC": "Phe", "UUA": "Leu", "UUG": "Leu",
     "CUU": "Leu", "CUC": "Leu", "CUA": "Leu", "CUG": "Leu", "AUU": "Iso",
     "AUC": "Iso", "AUA": "Iso", "ACU": "Thr", "ACC": "Thr", "ACA": "Thr",
@@ -173,8 +175,9 @@ codon_to_aminoacid = {
     "GCU": "Ala", "GCC": "Ala", "GCA": "Ala", "GCG": "Ala", "AAU": "Asn",
     "AAC": "Asn", "AAA": "Lys", "AAG": "Lys", "GAU": "Asp", "GAC": "Asp",
     "GAA": "Glu", "GAG": "Glu", "UGU": "Cys", "UGC": "Cys", "UGA": "Stop",
-    "UG
-
+    "UGG": "Trp", "CGU": "Arg", "CGC": "Arg", "CGA": "Arg", "CGG": "Arg",
+    "AGU": "Ser", "AGC": "Ser", "AGA": "Arg", "AGG": "Arg", "GGU": "Gly",
+    "GGC": "Gly", "GGA": "Gly", "GGG": "Gly"
 }
             st.error("La secuencia de ARN contiene bases inválidas. Solo se permiten A, U, G y C.")
     else:
