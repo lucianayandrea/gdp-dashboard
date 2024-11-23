666c5ac5096684b7cbd1c21a6328454ad88704a3
lucianayandrea <lucirey04@gmail.com>
2024-11-22 19:10:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -332,7 +332,6 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import Counter
-pip install matplotlib
 
 
 # Función para contar la frecuencia de los aminoácidos en la cadena ingresada
